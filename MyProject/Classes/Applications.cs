d099418a28b42eab73a8450c6f5e7a599883e465
Masud <amirkhonov.masud90@gmail.com>
2020-04-23 19:57:18 +0300

    Getting info about Client by ID !

--- MyProject/Classes/Applications.cs
+++ MyProject/Classes/Applications.cs
@@ -35,7 +35,43 @@ namespace MyProject
             int clientId = ID;
             
             double salary = this.Salary;
+
+            //! Getting info about Client by ID 
+            SqlConnection connection = new SqlConnection(Connection.connectionString);
+
+            if ( connection.State == ConnectionState.Open ) {
+                connection.Close();
+            }
+            connection.Open();
+
+            string commandText = $"Select * from Client where ID = {clientId}";
+            SqlCommand command = new SqlCommand(commandText, connection);
+            SqlDataReader reader = command.ExecuteReader(); 
+
+            string Firstname = " ", Secondname=" ", Gender =" ", Citizenship = " ", Family = " ", CreditGoal = " ";
+            int Age = 0, CreditTerm = 0;
+            double Salary = 0.0, CreditAmount = 0.0; 
+
+            CreditAmount = Amount;
+            CreditTerm = Term;
+            CreditGoal = Goal;
+            while (reader.Read()) {
+                Firstname = reader.GetValue(1).ToString().Trim();
+                Secondname = reader.GetValue(2).ToString().Trim();
+                Gender = reader.GetValue(4).ToString().Trim();
+                Age = int.Parse(reader.GetValue(5).ToString().Trim()); 
+                Citizenship = reader.GetValue(6).ToString().Trim(); 
+                Family = reader.GetValue(7).ToString().Trim(); 
+            }
+
+            reader.Close();
+
             
+
+
+
+
+
 
 
         
