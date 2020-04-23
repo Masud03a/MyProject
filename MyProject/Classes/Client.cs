2f3005e70d60f984f7cb3c4e403fe89c31b4ed81
Masud <amirkhonov.masud90@gmail.com>
2020-04-23 19:15:38 +0300

    Show by client id!

--- MyProject/Classes/Client.cs
+++ MyProject/Classes/Client.cs
@@ -13,6 +13,7 @@ namespace MyProject
         private string Gender{get; set;}
         private int Age{get; set;}
         private string Citizenship {get; set;}
+        private string Family{get; set;} 
 
         public Client(int ID) {
             this.ID = ID; 
@@ -33,7 +34,50 @@ namespace MyProject
             this.Family = Family;
         }
 
+        public void showAllClients() {
+            SqlConnection connection = new SqlConnection(Connection.connectionString);
+            if ( connection.State == ConnectionState.Open ){
+                connection.Close();
+            }
+            connection.Open();
 
-        private string Family{get; set;} 
+            string commandText = $"Select ID, Firstname, Secondname from Client";
+            
+            SqlCommand command = new SqlCommand(commandText, connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            System.Console.WriteLine("ID\t\tFirstname\t\tSecondname");
+
+            while ( reader.Read() ) {
+                string tempId = reader.GetValue(0).ToString();
+                string tempFirstname = reader.GetValue(1).ToString().Trim();
+                string tempSecondname = reader.GetValue(2).ToString().Trim();
+                Console.Write(tempId + "\t\t" + tempFirstname);
+                for ( int i = 0; i < 9 - tempFirstname.Length; i++ ) {
+                    System.Console.Write(" ");
+                } 
+                Console.WriteLine("\t\t" + tempSecondname);
+            }
+            reader.Close(); 
+        }
+         public void showByClientId( int id ) {
+            SqlConnection connection = new SqlConnection(Connection.connectionString);
+            if ( connection.State == ConnectionState.Open ) {
+                connection.Close();
+            }
+            connection.Open();
+
+            string commandText = $"Select Firstname, Secondname from Client where ID = {id}"; 
+            
+            SqlCommand command = new SqlCommand(commandText, connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+            
+            adminFunctions: 
+                System.Console.WriteLine("What you want to see:");
+                System.Console.WriteLine("1. Applications");
+                System.Console.Write("2. Payment graph of Application By ID\n3. Exit\nPlease type reference number: ");
+            int adminChoice = int.Parse(Console.ReadLine());
     }
 }
